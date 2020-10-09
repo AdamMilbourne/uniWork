e0bb854aeb98092de790452cd41c94b1d7ff1e3d
Adam MILBOURNE <m016092k@student.staffs.ac.uk>
2020-10-09 13:11:01 +0100

    Comleted

---
+++ Program 3 Defining Variables (portfolio)/Program 3 Defining Variables (portfolio)/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	int myInt1 = 5, myInt2;
+	myInt2 = 10;
+
+	std::cout << "value stored in myInt1 is " << myInt1 << std::endl;
+	std::cout << "value stored in myInt2 is " << myInt2 << std::endl;
+
+
+	float myFloat1 = 1.2f, myFloat2;
+	myFloat2 = 9.9f;
+
+	std::cout << "value stored in myFloat1 is " << myFloat1 << std::endl;
+	std::cout << "value stored in myFloat2 is " << myFloat2 << std::endl;
+
+	char myChar = 'a';
+
+	std::cout << "value stored in myChar is " << myChar << std::endl;
+
+	return 0;
+}
