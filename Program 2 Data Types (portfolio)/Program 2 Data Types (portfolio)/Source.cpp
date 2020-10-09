8f5ebb6b3308c0483e2e03308940338e2fd6b2d2
Adam MILBOURNE <m016092k@student.staffs.ac.uk>
2020-10-09 11:56:21 +0100

    comleted

---
+++ Program 2 Data Types (portfolio)/Program 2 Data Types (portfolio)/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "size of char:" << sizeof(char) << "bytes" << std::endl;
+
+	std::cout << "size of int:" << sizeof(int) << "bytes" << std::endl;
+
+	std::cout << "size of short int:" << sizeof(short int) << "bytes" << std::endl;
+
+	std::cout << "size of long int:" << sizeof(long int) << "bytes" << std::endl;
+
+	std::cout << "size of float:" << sizeof(float) << "bytes" << std::endl;
+
+	std::cout << "size of double:" << sizeof(double) << "bytes" << std::endl;
+
+	std::cout << "size of wchar_t:" << sizeof(wchar_t) << "bytes" << std::endl;
+
+	return 0;
+}
