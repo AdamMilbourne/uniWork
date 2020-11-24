7e486765626c0f4a578fab5ad94c61f19a76c435
Adam MILBOURNE <m016092k@student.staffs.ac.uk>
2020-11-24 17:47:14 +0000

    Done

--- program 21/program 21/Source.cpp
+++ program 21/program 21/Source.cpp
@@ -1,10 +1,12 @@
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int findsmallestelement(int arr[], int n)
+
+int findsmallestelement(int arr[], int n, int i)
 {
-	int i = 0;
+	
 	int temp = arr[0];
 	for (i; i < n; i++)
 	{
@@ -13,13 +15,16 @@ int findsmallestelement(int arr[], int n)
 			temp = arr[i];
 		}
 	}
-	
-	cout << "in position: ";
+	cout << "smallest number is in position: " << i << endl;
+	cout << "position 1 2 3 4 5 6 7 8 9 10" << endl;
 	return temp;
 }
 
+
+
 int main()
 {
+	int i = 0;
 	const int n = 10;
 	int arr[n];
 
@@ -28,7 +33,12 @@ int main()
 		cout << "enter a number" << endl;
 		cin >> arr[i];
 	}
-	int smallest = findsmallestelement(arr, n);
+	int smallest = findsmallestelement(arr, n, i);
+	cout << setw(11);
+	for (int j = 0; j < 10; j++)
+	{
+		cout << arr[j] << setw(2);
+	}
 	cout << " The smallest number is: " << smallest;
 	return 0;
 	}
