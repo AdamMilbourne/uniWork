01d95449f34c00672d455a74be1e822561c63e7a
Adam MILBOURNE <m016092k@student.staffs.ac.uk>
2020-12-08 15:13:45 +0000

    completed

--- program 15/program 15/Source.cpp
+++ program 15/program 15/Source.cpp
@@ -1,111 +1,31 @@
 #include <iostream>
-
+#include <Windows.h>
+using namespace std;
 int main()
 {
-	int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
-	int PlayerInput;
-	char playerdecision;
-	playerdecision ='Y';
-
-
-	while (playerdecision == 'Y'|| playerdecision == 'y')
-	{
-	
-
-		//pick number
-		std::cout << "please enter a number between 1 and 10" << std::endl;
-
-		//players response
-		std::cin >> PlayerInput;
-
-		switch (PlayerInput)
-		{
-		case 1:
-			std::cout << "*" << std::endl;
-			break;
-		case 2:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			break;
-		case 3:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			std::cout << "***" << std::endl;
-			break;
-		case 4:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			std::cout << "***" << std::endl;
-			std::cout << "****" << std::endl;
-			break;
-		case 5:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			std::cout << "***" << std::endl;
-			std::cout << "****" << std::endl;
-			std::cout << "*****" << std::endl;
-			break;
-		case 6:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			std::cout << "***" << std::endl;
-			std::cout << "****" << std::endl;
-			std::cout << "*****" << std::endl;
-			std::cout << "******" << std::endl;
-			break;
-		case 7:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			std::cout << "***" << std::endl;
-			std::cout << "****" << std::endl;
-			std::cout << "*****" << std::endl;
-			std::cout << "******" << std::endl;
-			std::cout << "*******" << std::endl;
-			break;
-		case 8:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			std::cout << "***" << std::endl;
-			std::cout << "****" << std::endl;
-			std::cout << "*****" << std::endl;
-			std::cout << "******" << std::endl;
-			std::cout << "*******" << std::endl;
-			std::cout << "********" << std::endl;
-			break;
-		case 9:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			std::cout << "***" << std::endl;
-			std::cout << "****" << std::endl;
-			std::cout << "*****" << std::endl;
-			std::cout << "******" << std::endl;
-			std::cout << "*******" << std::endl;
-			std::cout << "********" << std::endl;
-			std::cout << "*********" << std::endl;
-			break;
-		case 10:
-			std::cout << "*" << std::endl;
-			std::cout << "**" << std::endl;
-			std::cout << "***" << std::endl;
-			std::cout << "****" << std::endl;
-			std::cout << "*****" << std::endl;
-			std::cout << "******" << std::endl;
-			std::cout << "*******" << std::endl;
-			std::cout << "********" << std::endl;
-			std::cout << "*********" << std::endl;
-			std::cout << "**********" << std::endl;
-			break;
-
-
-		}
-		std::cout << "would you like yo go again [Y or N]" << std::endl;
-		std::cin >> playerdecision;
-
-		if (playerdecision == 'N' || playerdecision == 'n')
-		{
-			std::cout << "goodbye" << std::endl;
-		}
-
-	}
+    char userinput = 'y';
+    while (userinput == 'y' || userinput == 'Y')
+    {
+        cout << "enter a number between 1 and 10.\n";
+        char star = '*';
+        int input;
+        char playAgain;
+        cin >> input;
+        for (int i = 0; i < input + 1; i++)
+        {
+            cout << endl;
+            for (int j = 0; j < i; j++)
+            {
+                cout << star;
+            }
+        }
+        cout << "\nwould you like to play again? press y for yes and n for no." << endl;
+        cin >> userinput;
+    }
+    if (userinput == 'n' || userinput == 'N')
+    {
+        cout << "\ngoodbye" << endl;
+        Sleep(1000);
+    }
 }
 
